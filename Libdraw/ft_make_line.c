6eba399e83a8526240ca5a8f6f195e28f698a768
Alexis PERRAULT <aperraul@e3r7p11.42.fr>
2016-01-27 13:32:21 +0100

    ONG LE BO CARRE

--- Libdraw/ft_make_line.c
+++ Libdraw/ft_make_line.c
@@ -6,7 +6,7 @@
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/25 16:14:21 by aperraul          #+#    #+#             */
-/*   Updated: 2016/01/25 18:26:41 by aperraul         ###   ########.fr       */
+/*   Updated: 2016/01/27 13:14:23 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ t_line		ft_make_line(int x1, int y1, int x2, int y2)
 	line.start = ft_make_pt(x1, y1);
 	line.end = ft_make_pt(x2, y2);
 	line.dx = x2 - x1;
-	line.dx = y2 - y1;
+	line.dy = y2 - y1;
 	return (line);
 }
--- Libdraw/main_test.c
+++ Libdraw/main_test.c
@@ -6,7 +6,7 @@
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/25 13:09:04 by aperraul          #+#    #+#             */
-/*   Updated: 2016/01/25 18:29:09 by aperraul         ###   ########.fr       */
+/*   Updated: 2016/01/27 13:29:57 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,48 @@ int		main()
 {
 	t_mlx	*mlx;
 	t_line	line;
+	int		x1;
+	int		y1;
+	int		x2;
+	int		y2;
 
-	mlx = ft_mlx_init(mlx, 400, 400, "FDF");
-	line = ft_make_line(100, 50, 230, 40);
-	ft_draw_line(line, mlx, 0x00FF00);
+
+	x1 = 300;
+	y1 = 300;
+	x2 = 600;
+	y2 = 300;
+	mlx = NULL;
+	mlx = ft_mlx_init(mlx, 800, 800, "FDF");
+	while (x2 > x1)
+	{
+	line = ft_make_line(x1, y1, x2, y2);
+	ft_draw_line(line, mlx, 0xffffff);
+	x2--;
+	y2--;
+	}
+	while (y2 < y1)
+	{
+	line = ft_make_line(x1, y1, x2, y2);
+	ft_draw_line(line, mlx, 0xffffff);
+	x2--;
+	y2++;
+	}
+	while (x2 < x1)
+	{
+	line = ft_make_line(x1, y1, x2, y2);
+	ft_draw_line(line, mlx, 0xffffff);
+	x2++;
+	y2++;
+	}
+	while (y2 > y1)
+	{
+	line = ft_make_line(x1, y1, x2, y2);
+	ft_draw_line(line, mlx, 0xffffff);
+	x2++;
+	y2--;
+	}
+
 	mlx_put_image_to_window(mlx->p_mlx, mlx->p_win, mlx->mlx_img->p_img, 0, 0);
-	mlx_loop(mlx);
+	mlx_loop(mlx->p_mlx);
+	return (0);
 }
