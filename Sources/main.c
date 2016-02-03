ce47d9f357c8922e7b3544302b0353471977cc6b
Alexis PERRAULT <aperraul@e3r7p13.42.fr>
2016-02-03 17:52:59 +0100

    go debug demain

--- Sources/main.c
+++ Sources/main.c
@@ -6,7 +6,7 @@
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/27 16:56:33 by aperraul          #+#    #+#             */
-/*   Updated: 2016/02/03 16:54:26 by aperraul         ###   ########.fr       */
+/*   Updated: 2016/02/03 17:46:17 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,28 @@
 int		main(int argc, char **argv)
 {
 	int			fd;
-	t_array		*a;
+	t_array		*array;
+	int			x;
+	int			y;
 
 
 	if (argc == 2)
 	{
-		a = NULL;
+		array = NULL;
 		if((fd = open(argv[1], O_RDONLY)) == -1)
 			return (0);
-		if(!(ft_create_array(a, fd)))
+		if(!(ft_create_array(array, fd)))
 			return (0);
+		y = 0;
+		while (y < array->y)
+		{
+			x = 0;
+			while (x < *array->nb_x_line[x])
+			{
+			ft_putnbr(array->tab[y][x]);
+			x++;
+			}
+		}
 	}
 	else
 		ft_putstr("erreur");
