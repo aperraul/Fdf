75228c8b38e26a775eeb8822f7bcd9af56fc64aa
Alexis PERRAULT <aperraul@e3r9p13.42.fr>
2016-02-22 15:20:52 +0100

    add exit

--- sources/ft_fdf_exit.c
+++ sources/ft_fdf_exit.c
@@ -6,7 +6,7 @@
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/14 16:06:30 by aperraul          #+#    #+#             */
-/*   Updated: 2016/02/14 18:11:21 by aperraul         ###   ########.fr       */
+/*   Updated: 2016/02/22 15:19:19 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_fdf_exit(t_fdf *fdf)
 {
 	ft_del_array(fdf->array);
 	ft_clear_mlx(fdf->mlx);
-	ft_memdel((void **)fdf);
+	ft_memdel((void **)&fdf);
 	exit(0);
 }
